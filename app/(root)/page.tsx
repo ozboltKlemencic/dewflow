80bceded6f2275319e6feef8f7b3019c737492f6
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2024-11-09 14:40:37 +0100

    Implement Github SignIn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <h1>s</h1>;
-}
+import React from "react";
+
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold">Home</h1>
+
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+        className="px-10 pt-[100px]"
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
