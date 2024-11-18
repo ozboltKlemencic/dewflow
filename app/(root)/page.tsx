d57231bab83ebedbce8cc439ecb72fba4f38b22f
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2024-11-18 18:05:17 +0100

    Base Home Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,31 @@
+import Link from "next/link";
 import React from "react";
 
-import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
   return (
     <>
-      <h1 className="h1-bold">Home</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Questions</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Search</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 };
