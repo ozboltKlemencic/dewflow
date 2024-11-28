4e0ffcdc7dfcb142fd16556f274b261429b4741b
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2024-11-28 18:45:36 +0100

    Introduction to Route Handlers

---
+++ app/database.ts
@@ -0,0 +1,22 @@
+const tickets = [
+  {
+    id: 1,
+    name: "Fix Next js route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Fix Next js route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 3,
+    name: "Fix Next js route bug",
+    status: "open",
+    type: "bug",
+  },
+];
+
+export default tickets;
